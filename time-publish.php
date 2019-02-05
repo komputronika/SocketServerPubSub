874f2e9909d67193f084a559b5198194d789ed47
Komputronika <piepin@gmail.com>
2019-02-06 00:22:08 +0700

    Publish time untuk topik demo

--- time-publish.php
+++ time-publish.php
@@ -3,29 +3,26 @@ $host = "127.0.0.1";
 $port = 4444;
 
 while (true) {
-	$fp = @stream_socket_client("tcp://$host:$port", $errno, $errstr, 30);
-	if (!$fp) {
-	    echo "$errstr ($errno)\n";
-	} else {
-		
-		$time = time();
+    $fp = @stream_socket_client("tcp://$host:$port", $errno, $errstr, 60);
+    if ($fp) {
+        $time  = time();
         $error = false;
-		while (!$error) {
-			// Publish data demo untuk keperluan testing
-		    if ( (time() - $time) >= 2) {
-		        $demo_data["action"] = "pub";
-		        $demo_data["topic"]  = "demo";
-		        $demo_data["data"]   = "Anda subscribe pada topik demo. Waktu: ".date("H:i:s");
-		        $status =  @fwrite($fp, json_encode($demo_data)."\n");
-				$time = time();
-		        
-		        if ($status === false) {
-		        	$error = true;
-		        }
-		    }
-		}
-		// Jeda sebentar, lalu ulang lagi untuk konek / loop while(true)
-		sleep(5);
-	}
+        while (!$error) {
+            // Publish data demo untuk keperluan testing
+            if ((time() - $time) >= 2) {
+                $demo_data["action"] = "pub";
+                $demo_data["topic"]  = "demo";
+                $demo_data["data"]   = "Anda subscribe pada topik demo. Waktu: " . date("H:i:s");
+                $status              = @fwrite($fp, json_encode($demo_data) . "\n");
+                $time                = time();
+
+                if ($status === false) {
+                    $error = true;
+                    @fclose($fp);
+                }
+            }
+        }
+        // Jeda sebentar, lalu ulang lagi untuk konek / loop while(true)
+        sleep(5);
+    }
 }
-?>
