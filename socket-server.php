41d840aa0e5de11e6917c6df9e601f06954c2f15
komputronika <piepin@gmail.com>
2019-02-06 08:39:59 +0700

    Penanganan multiline JSON, penambahan config dan notes

--- socket-server.php
+++ socket-server.php
@@ -13,13 +13,22 @@
  * @link       https://github.com/komputronika/SocketServerPubSub
  */
 
+// Konfigurasi
+$config["verbose"] = true; 
+$config["host"]    = "0.0.0.0"; 
+$config["port"]    = "4444"; 
+
+echo "JSON Socket Server\n";
+
 // Mulai socket server pada port yang ditentukan: 4444
-$server = stream_socket_server("tcp://0.0.0.0:4444", $errno, $errorMessage);
+$server = stream_socket_server("tcp://".$config["host"].":".$config["port"], $errno, $errorMessage);
 
 // Bila tidak dapat membuat socket, beritahukan error
 if ($server === false) 
 {
     die("Tidak dapat membuat socket. Error [$errno]: $errorMessage");
+} else {
+    echo "Aktif pada IP:".$config["host"].", port:".$config["port"]."\n";
 }
 
 // Variable untuk menyimpan semua data subscriber
@@ -67,7 +76,7 @@ while(true)
         if ($new_client) 
         {
             // Ini informasi IP and port client tersebut
-            echo 'Koneksi baru dari ' . stream_socket_get_name($new_client, true) . "\n";
+            notes($config, "Koneksi baru dari " . stream_socket_get_name($new_client, true));
 
             // Tambahkan ke data client yang aktif
             $client_socks[] = $new_client;
@@ -97,7 +106,7 @@ while(true)
             @fclose($sock);
 
             // Tampilkan informasi
-            echo "Sebuah client terputus, jumlah sekarang: ". count($client_socks) . " client.\n";
+            notes($config, "Sebuah client terputus, jumlah sekarang: ". count($client_socks) . " client");
 
             // Lanjutkan ke client berikutnya
             continue;
@@ -109,115 +118,130 @@ while(true)
             $client_info = stream_socket_get_name($sock,true);
 
             //echo "Data masuk dari $client_info: ".$data;
-            echo "Data masuk dari $client_info\n";
+            notes($config, "Data masuk dari $client_info");
 
-            // Bila koneksi datang dari WebSocket 
-            if (strpos($data, "HTTP")!==false ) {
+            // Pecah data bila ada string JSON lebih dari 1
+            $datas = explode("\n",$data);
 
-                // Baca data header
-                preg_match('#Sec-WebSocket-Key: (.*)\r\n#', $data, $matches);
-                $key = base64_encode(pack(
-                    'H*',
-                    sha1($matches[1] . '258EAFA5-E914-47DA-95CA-C5AB0DC85B11')
-                ));
+            // Loop pada semua string JSON hasil pecahan
+            foreach($datas as $data) {
 
-                // Buat response handshaking untuk browser
-                $headers = "HTTP/1.1 101 Switching Protocols\r\n";
-                $headers.= "Upgrade: websocket\r\n";
-                $headers.= "Connection: Upgrade\r\n";
-                $headers.= "Sec-WebSocket-Version: 13\r\n";
-                $headers.= "Sec-WebSocket-Accept: $key\r\n\r\n";
+                if (empty($data)) continue;
 
-                // Kirim response handshaking ke browser
-                fwrite($sock,$headers);
+                // Bila koneksi datang dari WebSocket 
+                if (strpos($data, "HTTP")!==false ) {
 
-            } else {
+                    // Baca data header
+                    preg_match('#Sec-WebSocket-Key: (.*)\r\n#', $data, $matches);
+                    $key = base64_encode(pack(
+                        'H*',
+                        sha1($matches[1] . '258EAFA5-E914-47DA-95CA-C5AB0DC85B11')
+                    ));
 
-                // Asumsi bila awalan data bukan '{', bukan JSON, 
-                // maka itu adalah data encoded dari websocket
-                if (strpos($data,"{")===false) {
-                    // Decode data websocket dari client
-                    $data = decode($data);
-                    // Set jenis clientnya adalah web = websocket
-                    $type = "web";
+                    // Buat response handshaking untuk browser
+                    $headers = "HTTP/1.1 101 Switching Protocols\r\n";
+                    $headers.= "Upgrade: websocket\r\n";
+                    $headers.= "Connection: Upgrade\r\n";
+                    $headers.= "Sec-WebSocket-Version: 13\r\n";
+                    $headers.= "Sec-WebSocket-Accept: $key\r\n\r\n";
+
+                    // Kirim response handshaking ke browser
+                    fwrite($sock,$headers);
+
+                } else {
+
+                    // Asumsi bila awalan data bukan '{', bukan JSON, 
+                    // maka itu adalah data encoded dari websocket
+                    if (strpos($data,"{")===false) {
+                        // Decode data websocket dari client
+                        $data = decode($data);
+                        // Set jenis clientnya adalah web = websocket
+                        $type = "web";
+                    }
                 }
-            }
 
-            // Ubah string data menjadi JSON
-            $json = (array) @json_decode($data);
-            //print_r($json);
+                // Ubah string data menjadi JSON
+                $json = (array) @json_decode($data);
+                //print_r($json);
 
-            // Periksa apakah hasil decode benar dan menjadi array
-            if (count($json)) 
-            {
-                // Contoh string subscribe dari client
-                // {"action":"sub", "topic":"relay"}
+                // Periksa apakah hasil decode benar dan menjadi array
+                if (count($json)) 
+                {
+                    // Contoh string subscribe dari client
+                    // {"action":"sub", "topic":"relay"}
 
-                // Bila action adalah 'sub', masukan client ke array subscriber
-                if ($json["action"] == "sub") {
+                    // Bila action adalah 'sub', masukan client ke array subscriber
+                    if ($json["action"] == "sub") {
 
-                    if (!empty($json["topic"]))
-                    {
-                        echo "Type = $type\n";
-                        $topic = $json["topic"];
-                        $client_data = array( "type" => $type, "socket" => $sock, "info" => $client_info );
-                        $subscriber["$topic"][] = $client_data;
-                        echo "Subscriber baru pada topik <".strtoupper($topic).">\n";
+                        if (!empty($json["topic"]))
+                        {
+                            notes($config, "Type = $type");
+                            $topic = $json["topic"];
+                            $client_data = array( "type" => $type, "socket" => $sock, "info" => $client_info );
+                            $subscriber["$topic"][] = $client_data;
+                            
+                            notes($config, "Subscriber baru pada topik <$topic>");
 
-                        // Tampilkan data subscriber
-                        echo "Data subscriber sekarang:\n";
+                            // Tampilkan data subscriber
+                            notes($config, "Data subscriber sekarang:");
 
-                        $n = 1;
-                        // Loop pada semua subscriber, semua topic
-                        foreach($subscriber as $topic => $sub) {
-                            // Tampilkan jumlah subscriber
-                            echo "$n) $topic => ".count($sub)." client\n";
+                            $n = 1;
+                            // Loop pada semua subscriber, semua topic
+                            foreach($subscriber as $topic => $sub) {
+                                // Tampilkan jumlah subscriber
+                                notes($config, "$n) $topic => ".count($sub)." client");
+                            }
+
+                        } else {
+
+                            notes($config, "Parameter topic belum diisi");
+                        }
+                    } 
+
+                    // Contoh string data publish dari client:
+                    // Data 1 dimensi =>  {"action":"pub", "topic":"relay", "data": "Hello World"}
+                    // Data berdimensi banyak => {"action":"pub", "topic":"kebunku", "data": 
+                    //                           { "suhu":38, "kelembaban":60 } }
+
+                    // Bila action adalah 'pub', publish data ke semua client yang subscribe
+                    if (@$json["action"] == "pub") {
+
+                        if (!empty($json["topic"]))
+                        {
+                            // Ambil data topic nya                 
+                            $topic = $json["topic"];
+                            
+                            // Buat array dengan key 'data' 
+                            $broadcast_data["data"] = $json["data"];
+
+                            // Publish ke semua subscriber dalam string JSON
+                            notes($config, "Publish baru pada topik <$topic>");
+                            $subscriber = publish($subscriber, $topic, json_encode($broadcast_data)."\n", $sent );
+                            notes($config, "Selesai mengirim publish pada topik <$topic> ke $sent subscriber");
+
+                        } else {
+
+                            notes($config, "Parameter topic belum diisi");
                         }
 
-                    } else {
-
-                        echo "Parameter topic belum diisi\n";
-                    }
-                } 
-
-                // Contoh string data publish dari client:
-                // Data 1 dimensi =>  {"action":"pub", "topic":"relay", "data": "Hello World"}
-                // Data berdimensi banyak => {"action":"pub", "topic":"kebunku", "data": 
-                //                           { "suhu":38, "kelembaban":60 } }
-
-                // Bila action adalah 'pub', publish data ke semua client yang subscribe
-                if (@$json["action"] == "pub") {
-
-                    if (!empty($json["topic"]))
-                    {
-                        // Ambil data topic nya                 
-                        $topic = $json["topic"];
-                        
-                        // Buat array dengan key 'data' 
-                        $broadcast_data["data"] = $json["data"];
-
-                        // Publish ke semua subscriber dalam string JSON
-                        echo "Publish baru pada topik <".strtoupper($topic).">\n";
-                        $subscriber = publish($subscriber, $topic, json_encode($broadcast_data)."\n" );
-                    
-                    } else {
-
-                        echo "Parameter topic belum diisi\n";
+                        // Contoh format yang diterima client / subscriber
+                        // Data 1 dimensi =>  {"data": "Hello World"}
+                        // Data berdimensi banyak => {"data": { "suhu":42, "kelembaban":60 } } 
                     }
 
-                    // Contoh format yang diterima client / subscriber
-                    // Data 1 dimensi =>  {"data": "Hello World"}
-                    // Data berdimensi banyak => {"data": { "suhu":42, "kelembaban":60 } } 
+                } else {
+
+                    // Kemungkinan bukan format JSON atau format JSON salah
+                    notes($config, "Data masuk bukan format JSON");
                 }
 
-            } else {
+            } // foreach($datas...
 
-                // Kemungkinan bukan format JSON atau format JSON salah
-                echo "Data masuk bukan format JSON\n";
-            }
-        }
-    }
-}
+        } // if (empty($data...
+
+    } // foreach($read_socks...
+
+} // while(true...
 
 // Tutup socket 
 socket_close($server);
@@ -230,12 +254,12 @@ socket_close($server);
  * @param  string $topic  Topik yang akan kirim
  * @return array  Subscriber 
  */
-function publish($sockets, $topic, $data) {
+function publish($sockets, $topic, $data, &$sent) {
     // Untuk semua element pada variable $socket
     if (@count($sockets["$topic"])<1) return;
 
     $n = 0;
-    $s = 0;
+    $sent = 0;
     foreach($sockets["$topic"] as $sock) {
         // Tulis data JSON ke resource socket 
         if ($sock["type"]=="tcp") {
@@ -256,11 +280,10 @@ function publish($sockets, $topic, $data) {
             unset($sockets["$topic"][$n]);
         } else { 
             // Hitung jumlah real yang terkirim
-            $s++; 
+            $sent++; 
         }
         $n++;
     }
-    echo "Selesai mengirim publish pada topik <".strtoupper($topic)."> ke $s subscriber\n";
 
     // Return array subscriber yang sudah dibersihkan
     return $sockets;
@@ -272,7 +295,7 @@ function publish($sockets, $topic, $data) {
  * @param  string $data Data yang diterima dari WebSocket
  * @return string
  */
-function decode($data){
+function decode($data) {
     $bytes = $data;
     $data_length = "";
     $mask = "";
@@ -299,7 +322,7 @@ function decode($data){
 /**
  * Encode data untuk dikirim via websocket
  *
- * @param  string $data Data yang akan dikirimken ke WebSocket dalam format teks JSON  
+ * @param  string $data Data yang akan dikirimkan ke WebSocket dalam format teks JSON  
  * @return string
  */
 function encode($data) {
@@ -315,6 +338,19 @@ function encode($data) {
         $header = pack('CCN', $b1, 127, $length);
 
     return $header.$data;
+}
+
+/**
+ * Menampilkan pesan di console 
+ *
+ * @param  string $config Konfigurasi  
+ * @param  string $str Teks yang akan ditampilkan  
+ * @return void
+ */
+function notes($config, $str) {
+    if (@$config["verbose"]) {
+        echo date("Y-m-d H:i:s")." ".$str."\n";
+    }
 }
 
 //--- akhir Script 
