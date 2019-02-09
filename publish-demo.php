e7973a7079c6fc95699f3112572aa710e5303b3f
Komputronika <piepin@gmail.com>
2019-02-09 12:49:08 +0700

    Publish data demo untuk keperluan test

---
+++ publish-demo.php
@@ -0,0 +1,26 @@
+<?php
+$host = "socket.komputronika.com";
+$port = 4444;
+
+while (true) {
+    $fp = @stream_socket_client("tcp://$host:$port", $errno, $errstr, 120);
+    if ($fp) {
+        $time  = time();
+        $error = false;
+        while (!$error) {
+            // Publish data demo untuk keperluan testing
+            $demo_data["action"] = "pub";
+            $demo_data["topic"]  = "demo";
+            $demo_data["data"]   = ["suhu"=>rand(20,30),"kelembaban"=>rand(30,80)];
+            $status              = @fwrite($fp, json_encode($demo_data) . "\n");
+
+            if ($status === false) {
+                $error = true;
+                @fclose($fp);
+            }
+            sleep(3);
+        }
+        // Jeda sebentar, lalu ulang lagi untuk konek / loop while(true)
+        sleep(5);
+    }
+}
